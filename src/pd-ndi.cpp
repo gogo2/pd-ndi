ea779e451a3e72a60a9215938ac8818173b49525
Bartek <barteks95@gmail.com>
2018-12-21 00:40:39 +0100

    cleanup

--- src/pd-ndi.cpp
+++ src/pd-ndi.cpp
@@ -5,46 +5,54 @@
 #define GLEW_STATIC
 
 #include <memory.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <string>
 #include "glew/glew.h"
 #include "m_pd.h"
-#include "pd-ndi.hpp"
+#include "NDISender.hpp"
 
 static t_class *pd_ndi_class;
 
+struct t_pd_ndi {
+    t_object x_obj;
+
+    NDISender *ndi_sender;
+
+};
+
 void *pd_ndi_new() {
     auto *pd_ndi = (t_pd_ndi *) pd_new(pd_ndi_class);
-    pd_ndi->ndi_connector = new PdGlNdiConnector(pd_ndi);
+    pd_ndi->ndi_sender = new NDISender(256, 256);
     return static_cast<void *>(pd_ndi);
 }
 
 void pd_ndi_delete(t_pd_ndi *pd_ndi) {
-    delete pd_ndi->ndi_connector;
+    delete pd_ndi->ndi_sender;
 }
 
-void pd_ndi_bang(t_pd_ndi *x) {
-    x->ndi_connector->bang();
+void pd_ndi_bang(t_pd_ndi *pd_ndi) {
+    for (int i = 0; i < 30; ++i) {
+        pd_ndi->ndi_sender->send_frame();
+        memset(pd_ndi->ndi_sender->p_data(), (i & 1) ? 255 : 0, 800 * 600 * 4);
+    }
 }
 
-void pd_ndi_resize_screen(t_pd_ndi *x, t_floatarg width, t_floatarg height) {
-    char buff[10];
-    x->ndi_connector->resize_screen((int) width, (int) height);
-    itoa((int) (width), buff, 10);
-    post(buff);
-    itoa((int) (height), buff, 10);
-    post(buff);
+void pd_ndi_resize_screen(t_pd_ndi *pd_ndi, t_floatarg width, t_floatarg height) {
+    pd_ndi->ndi_sender->resize_screen((int) width, (int) height);
+    post(std::to_string((int) width).c_str());
+    post(std::to_string((int) height).c_str());
 }
 
-void pd_ndi_set_framerate(t_pd_ndi *x, t_floatarg max_fps) {
-    char buff[10];
-    x->ndi_connector->set_framerate((int) max_fps);
+void pd_ndi_set_framerate(t_pd_ndi *pd_ndi, t_floatarg max_fps) {
+    pd_ndi->ndi_sender->set_framerate((int) max_fps);
     post("ndi max_fps:");
-    itoa((int) (max_fps), buff, 10);
-    post(buff);
+    post(std::to_string((int) max_fps).c_str());
 }
 
-void pd_ndi_send_framebuffer(t_pd_ndi *x, t_floatarg fbo) {
-    x->ndi_connector->send_framebuffer((int) fbo);
+void pd_ndi_send_framebuffer(t_pd_ndi *pd_ndi, t_floatarg tex) {
+    glBindTexture(GL_TEXTURE_2D, (GLuint) tex);
+    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, pd_ndi->ndi_sender->p_data());
+    pd_ndi->ndi_sender->send_frame();
 }
 
 extern "C" void pd_ndi_setup() {
@@ -61,30 +69,5 @@ extern "C" void pd_ndi_setup() {
     class_addfloat(pd_ndi_class, (t_method) pd_ndi_send_framebuffer);
     class_addmethod(pd_ndi_class, (t_method) pd_ndi_resize_screen, gensym("dimen"), A_DEFFLOAT, A_DEFFLOAT, 0);
     class_addmethod(pd_ndi_class, (t_method) pd_ndi_set_framerate, gensym("fps"), A_DEFFLOAT, 0);
-}
-
-PdGlNdiConnector::PdGlNdiConnector(t_pd_ndi *const pd_ndi) : pd_ndi_(pd_ndi), ndi_sender_{800, 600} {
-    post("connector created");
-}
-
-void PdGlNdiConnector::bang() {
-    for (int i = 0; i < 300; ++i) {
-        ndi_sender_.send_frame();
-        memset(ndi_sender_.p_data(), (i & 1) ? 255 : 0, 800 * 600 * 4);
-    }
-}
-
-void PdGlNdiConnector::resize_screen(int screen_width, int screen_height) noexcept {
-    ndi_sender_.resize_screen(screen_width, screen_height);
-}
-
-void PdGlNdiConnector::set_framerate(int max_fps) noexcept {
-    ndi_sender_.set_framerate(max_fps);
-}
-
-void PdGlNdiConnector::send_framebuffer(int tex) {
-    glBindTexture(GL_TEXTURE_2D, (GLuint) tex);
-    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, ndi_sender_.p_data());
-    ndi_sender_.send_frame();
 }
 
--- src/pd-ndi.hpp
+++ src/pd-ndi.hpp
@@ -9,45 +9,7 @@
 
 #include "NDISender.hpp"
 
-struct _pd_ndi;
-typedef _pd_ndi t_pd_ndi;
 
-class PdGlNdiConnector {
-public:
-    explicit PdGlNdiConnector(t_pd_ndi *pd_ndi);
-
-    PdGlNdiConnector() = delete;
-
-    PdGlNdiConnector(const PdGlNdiConnector &) = delete;
-
-    PdGlNdiConnector(const PdGlNdiConnector &&) = delete;
-
-    PdGlNdiConnector &operator=(const PdGlNdiConnector &) = delete;
-
-    PdGlNdiConnector &operator=(const PdGlNdiConnector &&) = delete;
-
-    ~PdGlNdiConnector() = default;
-
-    void send_framebuffer(int tex);
-
-    void bang();
-
-    void resize_screen(int screen_width, int screen_height) noexcept;
-
-    void set_framerate(int max_fps) noexcept;
-
-private:
-    t_pd_ndi *const pd_ndi_;
-    NDISender ndi_sender_;
-
-};
-
-struct _pd_ndi {
-    t_object x_obj;
-
-    PdGlNdiConnector *ndi_connector;
-
-};
 
 
 #endif //PD_NDI_HPP
