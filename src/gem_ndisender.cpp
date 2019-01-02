f0e08901a573fc900ea8a7fe444f0bb410e730f8
bartosz_sobol <barteks95@gmail.com>
2019-01-02 16:26:15 +0100

    texture_2d

--- src/gem_ndisender.cpp
+++ src/gem_ndisender.cpp
@@ -19,16 +19,15 @@ namespace pdndi {
         GLint vport_dim[4];
 
         glGetIntegerv(GL_VIEWPORT, vport_dim);
-        glEnable(GL_TEXTURE_RECTANGLE_EXT);
         glGenTextures(1, &frame_tex);
-        glBindTexture(GL_TEXTURE_RECTANGLE_EXT, frame_tex);
-        glTexImage2D(GL_TEXTURE_RECTANGLE_EXT, 0, GL_RGBA8, vport_dim[2], vport_dim[3], 0, GL_BGRA, GL_UNSIGNED_BYTE,
+        glBindTexture(GL_TEXTURE_2D, frame_tex);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, vport_dim[2], vport_dim[3], 0, GL_BGRA, GL_UNSIGNED_BYTE,
                      nullptr);
-        glCopyTexSubImage2D(GL_TEXTURE_RECTANGLE_EXT, 0, 0, 0, vport_dim[0], vport_dim[1], vport_dim[2], vport_dim[3]);
+        glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, vport_dim[0], vport_dim[1], vport_dim[2], vport_dim[3]);
 
         if (frame_tex) {
             ndi_sender_.resize_screen(vport_dim[2], vport_dim[3]);
-            glGetTexImage(GL_TEXTURE_RECTANGLE_EXT, 0, GL_BGRA, GL_UNSIGNED_BYTE, ndi_sender_.p_video_frame_data());
+            glGetTexImage(GL_TEXTURE_2D, 0, GL_BGRA, GL_UNSIGNED_BYTE, ndi_sender_.p_video_frame_data());
             ndi_sender_.send_frame();
             glDeleteTextures(1, &frame_tex);
         }
