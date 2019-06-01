df42c35cfb440d8e1a399f164e8d27c342df0f98
Bartek <barteks95@gmail.com>
2019-06-01 22:15:36 +0200

    source finder v1

--- src/NDI_base/NDISourceFinder.cpp
+++ src/NDI_base/NDISourceFinder.cpp
@@ -3,4 +3,45 @@
 // Created by Bartek
 //
 
+
 #include "NDISourceFinder.hpp"
+
+NDISourceFinder::NDISourceFinder() : pNDI_find_(nullptr), no_sources_(0), p_sources_(nullptr) {
+    if (!NDIlib_initialize()) {
+        std::cerr << "Error initializing NDI\n";
+    }
+    pNDI_find_ = NDIlib_find_create_v2();
+    if (pNDI_find_ == nullptr) {
+        std::cerr << "Error initializing NDI finder\n";
+    }
+}
+
+NDISourceFinder::~NDISourceFinder() {
+    NDIlib_find_destroy(pNDI_find_);
+    NDIlib_destroy();
+}
+
+bool NDISourceFinder::find_sources() {
+    p_sources_ = NDIlib_find_get_current_sources(pNDI_find_, &no_sources_);
+    return no_sources_ > 0;
+}
+
+const NDIlib_source_t *NDISourceFinder::p_sources() {
+    return p_sources_;
+}
+
+const uint32_t NDISourceFinder::no_sources() {
+    return no_sources_;
+}
+
+std::ostream &operator<<(std::ostream &ostream, const NDISourceFinder &source_finder) {
+    if (source_finder.no_sources_ > 0) {
+        for (uint32_t i = 0; i < source_finder.no_sources_; i++) {
+            ostream << i << "\t" << source_finder.p_sources_[i].p_ndi_name << std::endl;
+        }
+    } else {
+        ostream << "No sources detected during last check" << std::endl;
+    }
+    return ostream;
+}
+
--- src/NDI_base/NDISourceFinder.hpp
+++ src/NDI_base/NDISourceFinder.hpp
@@ -6,8 +6,38 @@
 #ifndef PD_NDI_NDISOURCEFINDER_HPP
 #define PD_NDI_NDISOURCEFINDER_HPP
 
+#include <iostream>
+#include <NDI/Processing.NDI.Lib.h>
 
 class NDISourceFinder {
+public:
+
+    NDISourceFinder();
+
+    NDISourceFinder(const NDISourceFinder &) = delete;
+
+    NDISourceFinder(const NDISourceFinder &&) = delete;
+
+    NDISourceFinder &operator=(const NDISourceFinder &) = delete;
+
+    NDISourceFinder &operator=(const NDISourceFinder &&) = delete;
+
+    friend std::ostream& operator<<(std::ostream& ostream, const NDISourceFinder& source_finder);
+
+    ~NDISourceFinder();
+    
+    bool find_sources();
+
+    const NDIlib_source_t* p_sources();
+
+    const uint32_t no_sources();
+
+
+private:
+    NDIlib_find_instance_t pNDI_find_;
+    uint32_t no_sources_;
+    const NDIlib_source_t* p_sources_;
+
 
 };
 
